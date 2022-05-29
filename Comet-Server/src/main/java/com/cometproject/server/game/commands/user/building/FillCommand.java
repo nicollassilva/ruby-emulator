7c908ec7f6da321745b4f16fbe25ef2ab2a44187
IDjinn <42049518+IDjinn@users.noreply.github.com>
2022-05-29 11:17:48 -0300

    wip

---
+++ Comet-Server/src/main/java/com/cometproject/server/game/commands/user/building/FillCommand.java
@@ -0,0 +1,27 @@
+package com.cometproject.server.game.commands.user.building;
+
+import com.cometproject.server.config.Locale;
+import com.cometproject.server.game.commands.ChatCommand;
+import com.cometproject.server.network.sessions.Session;
+
+public class FillCommand extends ChatCommand {
+    @Override
+    public void execute(Session client, String[] params) {
+
+    }
+
+    @Override
+    public String getPermission() {
+        return "commands_command";
+    }
+
+    @Override
+    public String getParameter() {
+        return "";
+    }
+
+    @Override
+    public String getDescription() {
+        return Locale.getOrDefault("command.fill.description", "Preenche uma região com mobis.");
+    }
+}
