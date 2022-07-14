2adfacb8caffd84420e49f1b180932943ee93dbf
IDjinn <42049518+IDjinn@users.noreply.github.com>
2022-07-14 08:36:47 -0300

    feat: event alert cmd roomname,hostname and username

--- Comet-Server/src/main/java/com/cometproject/server/game/commands/staff/alerts/EventAlertCommand.java
+++ Comet-Server/src/main/java/com/cometproject/server/game/commands/staff/alerts/EventAlertCommand.java
@@ -23,29 +23,27 @@ public class EventAlertCommand extends ChatCommand {
         }
 
         final int roomId = client.getPlayer().getEntity().getRoom().getId();
-
         final String imageEvent = Locale.getOrDefault("image.event", "event");
-
-        final IMessageComposer msg = new AdvancedAlertMessageComposer(
-                Locale.get("command.eventalert.alerttitle"),
-                Locale.get("command.eventalert.message")
-                        .replace("%message%", this.merge(params))
-                        .replace("%username%", client.getPlayer().getData().getUsername()) + "<br><br><b> " + client.getPlayer().getData().getUsername() + "</b>",
-                Locale.get("command.eventalert.buttontitle"), "event:navigator/goto/" + roomId, imageEvent);
-
-        for (final ISession session : NetworkManager.getInstance().getSessions().getSessions().values()) {
-            if (session.getPlayer() != null && !session.getPlayer().getSettings().ignoreEvents()) {
-                session.send(msg);
-            }
-        }
-
         final Room room = client.getPlayer().getEntity().getRoom();
 
         room.getData().setAccess(RoomAccessType.OPEN);
         GameContext.getCurrent().getRoomService().saveRoomData(room.getData());
 
-        if (!CometExternalSettings.enableStaffMessengerLogs) return;
+        for (final ISession session : NetworkManager.getInstance().getSessions().getSessions().values()) {
+            if (session.getPlayer() != null && !session.getPlayer().getSettings().ignoreEvents()) {
+                final IMessageComposer msg = new AdvancedAlertMessageComposer(
+                        Locale.get("command.eventalert.alerttitle"),
+                        Locale.get("command.eventalert.message")
+                                .replace("%message%", this.merge(params))
+                                .replace("%username%", session.getPlayer().getData().getUsername())
+                                .replace("%hostname%", client.getPlayer().getData().getUsername())
+                                .replace("%roomname%",room.getData().getName()),
+                        Locale.get("command.eventalert.buttontitle"), "event:navigator/goto/" + roomId, imageEvent);
+                session.send(msg);
+            }
+        }
 
+        if (!CometExternalSettings.enableStaffMessengerLogs) return;
         this.logDesc = "Alerta de evento criado por %s no quarto '%b'"
                 .replace("%s", client.getPlayer().getData().getUsername())
                 .replace("%b", client.getPlayer().getEntity().getRoom().getData().getName());
