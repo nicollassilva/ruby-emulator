9f81f5b434236e19623ff12e7eee172e76f960ad
Snaiker <steveraki2000@gmail.com>
2022-10-09 04:53:39 +0100

    Wired change

--- Comet-Server/src/main/java/com/cometproject/server/game/rooms/objects/items/types/floor/wired/triggers/WiredTriggerStateChanged.java
+++ Comet-Server/src/main/java/com/cometproject/server/game/rooms/objects/items/types/floor/wired/triggers/WiredTriggerStateChanged.java
@@ -2,6 +2,7 @@ package com.cometproject.server.game.rooms.objects.items.types.floor.wired.trigg
 
 import com.cometproject.api.game.rooms.objects.data.RoomItemData;
 import com.cometproject.server.game.rooms.objects.entities.RoomEntity;
+import com.cometproject.server.game.rooms.objects.entities.types.PlayerEntity;
 import com.cometproject.server.game.rooms.objects.items.RoomItemFloor;
 import com.cometproject.server.game.rooms.objects.items.types.floor.wired.base.WiredTriggerItem;
 import com.cometproject.server.game.rooms.types.Room;
@@ -14,15 +15,23 @@ public class WiredTriggerStateChanged extends WiredTriggerItem {
     }
 
     public static boolean executeTriggers(RoomEntity entity, RoomItemFloor floorItem) {
-        System.out.println("[--------] execute trigger");
-
+        PlayerEntity playerEntity = ((PlayerEntity) entity);
         boolean wasExecuted = false;
 
         for (final WiredTriggerStateChanged wiredItem : getTriggers(entity.getRoom(), WiredTriggerStateChanged.class)) {
             if(wiredItem == null) continue;
 
             if (wiredItem.getWiredData().getSelectedIds().contains(floorItem.getId())) {
-                wasExecuted = wiredItem.evaluate(entity, floorItem);
+                if(!floorItem.getPosition().touching(entity.getPosition())) {
+                    entity.moveTo(floorItem.getPosition().squareInFront(floorItem.getRotation()).getX(), floorItem.getPosition().squareInFront(floorItem.getRotation()).getY());
+                    return false;
+                } else if (floorItem.getPosition().touching(entity.getPosition())) {
+                    entity.cancelWalk();
+                    entity.lookTo(playerEntity.getPosition().squareInFront(floorItem.getRotation()).getX(), playerEntity.getPosition().squareBehind(floorItem.getRotation()).getY());
+                    wasExecuted = wiredItem.evaluate(entity, floorItem);
+                }
+
+                //wasExecuted = wiredItem.evaluate(entity, floorItem);
             }
         }
 
