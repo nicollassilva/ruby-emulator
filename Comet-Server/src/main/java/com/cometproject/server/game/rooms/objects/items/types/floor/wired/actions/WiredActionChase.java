e2b7cceaabf9ee62a35968743319387cbf373cc5
Paulo <pvictorlv@gmail.com>
2022-10-12 02:31:31 -0300

    Update WiredActionChase.java

--- Comet-Server/src/main/java/com/cometproject/server/game/rooms/objects/items/types/floor/wired/actions/WiredActionChase.java
+++ Comet-Server/src/main/java/com/cometproject/server/game/rooms/objects/items/types/floor/wired/actions/WiredActionChase.java
@@ -8,36 +8,21 @@ import com.cometproject.server.game.rooms.objects.entities.pathfinding.Pathfinde
 import com.cometproject.server.game.rooms.objects.entities.pathfinding.Square;
 import com.cometproject.server.game.rooms.objects.entities.pathfinding.types.ItemPathfinder;
 import com.cometproject.server.game.rooms.objects.entities.types.PlayerEntity;
+import com.cometproject.server.game.rooms.objects.items.RoomItemFactory;
 import com.cometproject.server.game.rooms.objects.items.RoomItemFloor;
 import com.cometproject.server.game.rooms.objects.items.types.floor.wired.base.WiredActionItem;
 import com.cometproject.server.game.rooms.objects.items.types.floor.wired.events.WiredItemEvent;
 import com.cometproject.server.game.rooms.objects.items.types.floor.wired.triggers.WiredTriggerCollision;
-import com.cometproject.server.game.rooms.objects.items.types.floor.wired.triggers.custom.WiredTriggerCollisionPlayer;
 import com.cometproject.server.game.rooms.types.Room;
 import com.cometproject.server.game.rooms.types.mapping.RoomTile;
 import com.cometproject.server.network.messages.outgoing.room.items.SlideObjectBundleMessageComposer;
-import com.cometproject.server.network.messages.outgoing.room.items.UpdateFloorItemMessageComposer;
 import com.cometproject.server.utilities.RandomUtil;
-import com.cometproject.server.utilities.comparators.PositionComparator;
 
-import javax.annotation.Nullable;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
-
 public class WiredActionChase extends WiredActionItem {
-    private static final int[] MOVE_DIR = new int[]{0,1,2,3};
-    public static final int CHASE_RADIUS = 4;
-    public static final double MOVEMENT_RANDOM_CHANCE = 35;
     private int targetId = -1;
 
-    /**
-     * Wired action to chase room entity
-     *
-     * @param itemData the item data
-     * @param room     the room
-     */
     public WiredActionChase(RoomItemData itemData, Room room) {
         super(itemData, room);
     }
@@ -53,94 +38,99 @@ public class WiredActionChase extends WiredActionItem {
     }
 
     @Override
-    public void onEventComplete(WiredItemEvent event) {
+    public boolean evaluate(RoomEntity entity, Object data) {
+        if (this.getWiredData().getSelectedIds().size() == 0) return false;
+
+        if (this.getWiredData().getDelay() >= 1) {
+            this.setTicks(RoomItemFactory.getProcessTime(this.getWiredData().getDelay() / 2));
+        } else {
+            this.onTickComplete();
+        }
+        return true;
+    }
+
+    @Override
+    public void onTickComplete(){
         if (this.getWiredData().getSelectedIds().size() == 0) return;
 
         for (long itemId : this.getWiredData().getSelectedIds()) {
-            final RoomItemFloor floorItem = this.getRoom().getItems().getFloorItem(itemId);
-            if (floorItem == null)
-                continue;
+            RoomItemFloor floorItem = this.getRoom().getItems().getFloorItem(itemId);
+            if (floorItem == null) continue;
 
-            final List<PlayerEntity> entities = getNearestPlayerEntitiesInRadius(floorItem, CHASE_RADIUS);
-            boolean collision = false;
-            for (final PlayerEntity entity : entities) {
-                if (isCollided(floorItem, entity)) { // call colision trigger and skip for dont waste resources
-                    WiredTriggerCollision.executeTriggers(entity, floorItem);
-                    collision = true;
+            PlayerEntity nearestEntity = floorItem.nearestPlayerEntity();
+            Position positionFrom = floorItem.getPosition().copy();
+
+            if (nearestEntity != null) {
+                if (this.isCollided(nearestEntity, floorItem)) {
+                    floorItem.setCollision(nearestEntity);
+                    WiredTriggerCollision.executeTriggers(nearestEntity, floorItem);
                 }
-            }
 
-            if(collision){
-                continue;
-            }
+                this.targetId = nearestEntity.getId();
+                List<Square> tilesToEntity = ItemPathfinder.getInstance().makePath(floorItem, nearestEntity.getPosition(), RoomDiagonalType.DISABLED.getKey(), false);
 
-            final List<Square> nearestEntityPath = getNearestEntityPath(floorItem, entities);
-            if (nearestEntityPath.size() == 0) {
-                moveFloorItemRandomly(floorItem);
-                continue;
-            }
-
-            final Position currentPosition = floorItem.getPosition().copy();
-            final Square nextSquare = nearestEntityPath.get(0);
-            final Position nextPosition = new Position(nextSquare.x, nextSquare.y);
-            floorItem.setMoveDirection(Position.calculateRotation(currentPosition, nextPosition));
-            if (this.getRoom().getItems().moveFloorItemWired(floorItem, nextPosition, floorItem.getRotation(), true, true)) {
-                this.getRoom().getEntities().broadcastMessage(new SlideObjectBundleMessageComposer(currentPosition, nextPosition, this.getVirtualId(), 0, floorItem.getVirtualId()));
-            } else {
-                moveFloorItemRandomly(floorItem);
-            }
-        }
-    }
-
-    private List<Square> getNearestEntityPath(RoomItemFloor item, List<PlayerEntity> entities) {
-        if (entities.size() == 0) return new ArrayList<>();
-        for (final PlayerEntity entity : entities) {
-            targetId = entity.getId();
-            final List<Square> path = ItemPathfinder.getInstance().makePath(item, entity.getPosition(), RoomDiagonalType.DISABLED.getKey(), false);
-            if (path.size() > 0) {
-                return path;
-            }
-        }
-
-        return new ArrayList<>();
-    }
-
-    private List<PlayerEntity> getNearestPlayerEntitiesInRadius(RoomItemFloor item, int radius) {
-        final List<PlayerEntity> entities = new ArrayList<>();
-        for (int x = item.getPosition().getX() - radius; x < item.getPosition().getX() + radius; x++) {
-            for (int y = item.getPosition().getY() - radius; y < item.getPosition().getY() + radius; y++) {
-                for (final RoomEntity entity : this.getRoom().getEntities().getEntitiesAt(new Position(x, y))) {
-                    if (entity instanceof PlayerEntity) {
-                        entities.add((PlayerEntity) entity);
+                for (int direction : Position.COLLIDE_TILES) {
+                    if (tilesToEntity.size() == 0.0) {
+                        tilesToEntity = ItemPathfinder.getInstance().makePath(floorItem, nearestEntity.getPosition().squareInFront(direction), RoomDiagonalType.DISABLED.getKey(), false);
                         break;
                     }
                 }
+
+                if (tilesToEntity != null && tilesToEntity.size() != 0) {
+                    Position positionTo = new Position(tilesToEntity.get(0).x, tilesToEntity.get(0).y);
+
+                    this.moveToTile(floorItem, positionFrom, positionTo);
+                    tilesToEntity.clear();
+                } else {
+                    this.moveToTile(floorItem, positionFrom, null);
+                }
+            } else {
+                this.moveToTile(floorItem, positionFrom, null);
             }
         }
+    }
 
-        final PositionComparator positionComparator = new PositionComparator(this);
-        entities.sort(positionComparator);
-        return entities;
+    public boolean isCollided(PlayerEntity entity, RoomItemFloor floorItem) {
+        boolean tilesTouching = entity.getPosition().touching(floorItem.getPosition());
+        if (tilesTouching) {
+            final boolean xMatches = entity.getPosition().getX() == floorItem.getPosition().getX();
+            final boolean yMatches = entity.getPosition().getY() == floorItem.getPosition().getY();
+
+            if (!xMatches && !yMatches) {
+                return false;
+            }
+            return true;
+        }
+        return false;
+    }
+
+    private void moveToTile(RoomItemFloor floorItem, Position from, Position to) {
+        if (to == null) {
+            for (int i = 0; i < 16; i++) {
+                if (to != null) break;
+                to = this.random(floorItem, from);
+            }
+            if (to == null) return;
+        }
+        if (this.getRoom().getItems().moveFloorItem(floorItem.getId(), to, floorItem.getRotation())) {
+            to.setZ(floorItem.getPosition().getZ());
+            this.getRoom().getEntities().broadcastMessage(new SlideObjectBundleMessageComposer(from, to, 0, 0, floorItem.getVirtualId()));
+        }
+        floorItem.nullifyCollision();
+    }
+
+    private Position random(RoomItemFloor floorItem, Position from) {
+        int randomDirection = RandomUtil.getRandomInt(0, 3) * 2;
+        Position newPosition = from.squareInFront(randomDirection);
+        RoomTile tile = floorItem.getRoom().getMapping().getTile(newPosition.getX(), newPosition.getY());
+
+        if (tile != null && tile.isReachable(floorItem)) {
+            return newPosition;
+        }
+        return null;
     }
 
     public int getTargetId() {
-        return targetId;
-    }
-
-
-    private void moveFloorItemRandomly(RoomItemFloor item) {
-        /* TODO: isso ainda nao está pronto. na versao final, ele ira andar em uma determinada direção
-         e randomicamente alterar a direção. mas até agora está ok
-         */
-
-        final Position currentPosition = item.getPosition().copy();
-        final Position newPosition = WiredActionMoveRotate.getRandomPosition(1, currentPosition, this.getRoom());
-        if (this.getRoom().getItems().moveFloorItemWired(item, newPosition, item.getRotation(), true, true)) {
-            this.getRoom().getEntities().broadcastMessage(new SlideObjectBundleMessageComposer(currentPosition, newPosition, 0, 0, item.getVirtualId()));
-        }
-    }
-
-    private static boolean isCollided(RoomItemFloor item, RoomEntity entity){
-        return item.getPosition().equals(entity.getPosition()) || item.getPosition().distanceTo(entity.getPosition()) == 1;
+        return this.targetId;
     }
 }
