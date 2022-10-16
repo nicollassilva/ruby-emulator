eb15dd73f6d6e10d311ae5362623156def648f25
nicollassilva <lyod.hp@gmail.com>
2022-10-15 23:25:02 -0300

    Improving GiveHanditem event to move the provider player into receiving player

--- Comet-Server/src/main/java/com/cometproject/server/network/messages/incoming/room/action/GiveHandItemMessageEvent.java
+++ Comet-Server/src/main/java/com/cometproject/server/network/messages/incoming/room/action/GiveHandItemMessageEvent.java
@@ -1,5 +1,6 @@
 package com.cometproject.server.network.messages.incoming.room.action;
 
+import com.cometproject.api.game.utilities.Position;
 import com.cometproject.server.game.rooms.objects.entities.types.PlayerEntity;
 import com.cometproject.server.network.messages.incoming.Event;
 import com.cometproject.server.network.sessions.Session;
@@ -9,25 +10,29 @@ import com.cometproject.server.protocol.messages.MessageEvent;
 public class GiveHandItemMessageEvent implements Event {
     @Override
     public void handle(Session client, MessageEvent msg) throws Exception {
-        int userId = msg.readInt();
+        final int userId = msg.readInt();
 
-        if (client.getPlayer().getEntity() == null || client.getPlayer().getEntity().getRoom() == null) {
+        if (client.getPlayer().getEntity() == null || client.getPlayer().getEntity().getRoom() == null)
+            return;
+
+        if (!client.getPlayer().getEntity().isVisible())
+            return;
+
+        final PlayerEntity providerEntity = client.getPlayer().getEntity();
+        final PlayerEntity receivingEntity = client.getPlayer().getEntity().getRoom().getEntities().getEntityByPlayerId(userId);
+
+        if (receivingEntity == null)
+            return;
+
+        if(!providerEntity.getPosition().touching(receivingEntity.getPosition())) {
+            if(!providerEntity.getPlayer().getEntity().isFreeze() && !providerEntity.hasAttribute("interacttpencours") && !providerEntity.hasAttribute("tptpencours")) {
+                final Position sqInFront = receivingEntity.getPosition().squareInFront(receivingEntity.getBodyRotation());
+
+                providerEntity.moveTo(sqInFront.getX(), sqInFront.getY());
+            }
+
             return;
         }
-
-        if (!client.getPlayer().getEntity().isVisible()) {
-            return;
-        }
-
-        PlayerEntity providerEntity = client.getPlayer().getEntity();
-        PlayerEntity receivingEntity = client.getPlayer().getEntity().getRoom().getEntities().getEntityByPlayerId(userId);
-
-        if (receivingEntity == null) {
-            return;
-        }
-
-        if (providerEntity.getPosition().distanceTo(receivingEntity.getPosition()) > 1.5)
-            return;
 
         receivingEntity.carryItem(providerEntity.getHandItem());
         providerEntity.carryItem(0);
